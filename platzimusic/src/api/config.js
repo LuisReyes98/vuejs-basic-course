6f6fd09a2b7ffafadb785ab31d5d6e92a81d8bf4
Luis Reyes <luisrogelio98@gmail.com>
2019-06-24 23:36:11 -0400

    initial commit

---
+++ platzimusic/src/api/config.js
@@ -0,0 +1,13 @@
+export default {
+    appName: 'platzimusic',
+    apiKey: '49f62e5ec9fe7e9b5eeb30f9586bc474',
+    secret: '860a8fd0d99cb68c463efcf87b3ec812',
+    registeredTo: 'luis_reyesh',
+}
+
+
+
+// Application name platzimusic
+// API key 49f62e5ec9fe7e9b5eeb30f9586bc474
+// Shared secret 860 a8fd0d99cb68c463efcf87b3ec812
+// Registered to luis_reyesh
